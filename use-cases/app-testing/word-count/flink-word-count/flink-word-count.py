b6bc342b9bfa1655ea934c422a1fa9d6cc29b556
callumHub <cl780851@dal.ca>
2023-07-15 19:15:38 +0000

    Fixed not logging issue, 'client side default logging level is warning'

--- use-cases/app-testing/word-count/flink-word-count/flink-word-count.py
+++ use-cases/app-testing/word-count/flink-word-count/flink-word-count.py
@@ -18,13 +18,13 @@ log_file_path = "../logs/output/flink.log"
 IN_TOPIC = "inTopic" #Same as in spark
 OUT_TOPIC = "outTopic"
 logging.basicConfig(filename=log_file_path, level=logging.INFO, \
-        format="%(asctime)s %(levelname)s: %(message)s")
+        format="%(asctime)s INFO: %(message)s")# Note after entering word count. Lowest log level is Warning
 logging.info("input: "+IN_TOPIC)
 logging.info("output: "+ OUT_TOPIC)
 
 def word_count(input_path, output_path):
     env = StreamExecutionEnvironment.get_execution_environment()
-    logging.info("created environment")
+    logging.warning("created environment")
     env.set_runtime_mode(RuntimeExecutionMode.STREAMING)
     # write all the data to one file
     env.set_parallelism(1)
@@ -36,14 +36,14 @@ def word_count(input_path, output_path):
 	.set_group_id("group_id") \
 	.build()
 
-    logging.info("connected to kafka source at:", kafka_node)	
-    
+    logging.warning("connected to kafka source at:"+kafka_node)	
+    logging.warning("Group ID is 'group_id'")
     ds = env.from_source(
 	source = kafka_source,
 	watermark_strategy = WatermarkStrategy.for_monotonous_timestamps(),
 	source_name = "kafka_source"
     ) 
-
+    logging.warning("Source name is 'kafka_source'")
     def split(line):
         yield from line.split()
 
@@ -53,7 +53,7 @@ def word_count(input_path, output_path):
         .key_by(lambda i: i[0]) \
         .reduce(lambda i, j: (i[0], i[1] + j[1])) \
         .map(lambda i: f'{i[0]}: {i[1]}', output_type = Types.STRING())
-
+    logging.warning("computing wordcount")
     # define the sink
     serializer = KafkaRecordSerializationSchema.builder().set_topic(OUT_TOPIC).set_value_serialization_schema(SimpleStringSchema()).build()
 	
@@ -63,8 +63,9 @@ def word_count(input_path, output_path):
 	  .set_record_serializer(serializer) \
           .build()
     ds.sink_to(sink)
-    logging.info("executing")
-    logging.info("Output to broker at", kafka_node)
+    logging.warning("Connected to kafka sink at: "+kafka_node)
+    logging.warning("executing")
+    logging.warning("Output to broker at: "+ kafka_node)
     # submit for execution
     env.execute()
 if __name__ == '__main__':
