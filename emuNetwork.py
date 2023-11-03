c139b9d5c8f81d270f7ba05f4a4f727067767a4b
ifath <monzurulamin10@gmail.com>
2023-11-03 15:29:07 -0300

    Accuracy analysis done for military-coordination application.

--- emuNetwork.py
+++ emuNetwork.py
@@ -39,7 +39,7 @@ class CustomTopo(Topo):
 			if 'bandwidth' in data:
 				linkBandwidth = int(data['bandwidth'])
 
-			linkDelay = '1ms'
+			linkDelay = "1s"
 			if 'latency' in data:
 				linkDelay = str(data['latency'])+'ms'
 
